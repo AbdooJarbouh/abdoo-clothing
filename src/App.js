47597aa8b827ffa207a442fa70b80dfb572d562e
AbdooJarbouh <99077311+AbdooJarbouh@users.noreply.github.com>
2022-02-26 01:40:56 +0300

    Directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
 
 function App() {
   return (
     <div >
       
-      <HomePage  hello world />
+      <HomePage />
       
     </div>
   );
