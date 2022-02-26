e97d7f128093ac512e3e10f116ecf26d09dc631f
AbdooJarbouh <99077311+AbdooJarbouh@users.noreply.github.com>
2022-02-26 04:22:44 +0300

    implemnting react-router to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
+import { Switch,Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+
+);
 
 function App() {
   return (
-    <div >
-      
-      <HomePage />
-      
-    </div>
+    
+    <Switch>
+      <div >
+        <Route exact path ='/' component={HomePage} />
+        <Route exact path ='/shop/hats' component={HatsPage} />
+      </div>
+    </Switch>
   );
 }
 
