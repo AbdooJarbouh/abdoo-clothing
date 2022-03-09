67888e62a3589e5855ea6bf269863b267974c520
AbdooJarbouh <99077311+AbdooJarbouh@users.noreply.github.com>
2022-03-09 03:26:33 +0300

    created cart-item component and connected it with reduser

--- src/REDUX/Cart/cart.utiles.js
+++ src/REDUX/Cart/cart.utiles.js
@@ -12,19 +12,3 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 };
-/* 
-export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-      cartItem => cartItem.id === cartItemToAdd.id
-    );
-  
-    if (existingCartItem) {
-      return cartItems.map(cartItem =>
-        cartItem.id === cartItemToAdd.id
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem
-      );
-    }
-  
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-  }; */
