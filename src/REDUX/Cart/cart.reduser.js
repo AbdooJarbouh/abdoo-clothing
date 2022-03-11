20a88694696c10cf3dd4d8e61bbee07747575bc4
Your Name <abdoojarboo@gmail.com>
2022-03-12 02:02:47 +0300

    finished the checkout page

--- src/REDUX/Cart/cart.reduser.js
+++ src/REDUX/Cart/cart.reduser.js
@@ -1,5 +1,5 @@
 import CartActionsTypes from './cart.types';
-import { addItemToCart } from './cart.utiles';
+import { addItemToCart,removeItemFromCart} from './cart.utiles';
 const INITIAL_STATE = {
     hidden: true,
     cartItems: [],
@@ -11,12 +11,26 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         case CartActionsTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: addItemToCart(state.cartItems,action.payload)
-            }
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        
+            
+            case CartActionsTypes.REMOVE_ITEM:
+                return {
+                    ...state,
+                    cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        
+        
+                case CartActionsTypes.CLEAR_ITEM_FROM_CART:
+                    return {
+                        ...state, cartItems: state.cartItems.filter(
+                            cartItem => cartItem.id !== action.payload.id)
+                    };
         default:
             return state;
     }
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItemFromCart , addItem , removeItem } from '../../REDUX/Cart/cart.actions';
+
 
 import './checkout-item.styles.scss';
 
-const CheckOutItem = ({ cartItem: { name, price, quantity, imageUrl } }) => (
+const CheckOutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+    const { name, price, quantity, imageUrl } = cartItem;
+    return (
 
-    <div className="checkout-item">
-        <div className="image-container">
-            <img src={imageUrl} alt="Item" />
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={imageUrl} alt="Item" />
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={ () => removeItem(cartItem)}>&#10094; </div>
+              <span className="value"> {quantity} </span> 
+                <div className="arrow" onClick={ () => addItem(cartItem)}>&#10095; </div>
+            </span>
+            <span className="price"> ${price} </span>
+            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005; </div>
         </div>
-        <span className="name">{name}</span>
-        <span className="quantity"> {quantity}</span>
-        <span className="price"> ${price} </span>
-        <div className="remove-button">&#10005; </div>
-    </div>
-);
+    )
+};
+
+
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+});
+
     
-export default CheckOutItem;
+export default connect(
+   null, mapDispatchToProps
+)(
+    CheckOutItem
+);
